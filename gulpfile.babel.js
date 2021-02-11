16f15a3e10a6672781e0bc5b65fd103a7f74aadf
JungHyoMoon <dsinhome70@gmail.com>
2021-02-11 19:37:14 +0900

    install all dependencies

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,2 +1,15 @@
 import gulp from "gulp";
+import del from "del";
+import server from "gulp-webserver";
 import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minify from "gulp-csso";
+import img from "gulp-image";
+import babelify from "babelify";
+import uglify from "uglifyify";
+
+const prepare = gulp.series([]);
+const assets = gulp.series([]);
+const live = gulp.parallel([]);
+
+export const dev = gulp.series([]);
