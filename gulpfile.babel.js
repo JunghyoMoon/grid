2178cda51ba3455e6b7dbf51837c7e5cbff1908e
JungHyoMoon <dsinhome70@gmail.com>
2021-02-11 21:47:18 +0900

    routes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,8 +8,25 @@ import img from "gulp-image";
 import babelify from "babelify";
 import uglify from "uglifyify";
 
+const routes = {
+    img: {
+        src: "src/img/*",
+        dest: "dest/img",
+    },
+    scss: {
+        watch: "src/scss/**/*.scss",
+        src: "src/scss/style.scss",
+        dest: "dest/css",
+    },
+    js: {
+        watch: "src/js/**/*.js",
+        src: "src/js/script.js",
+        dest: "dest/js",
+    },
+};
+
 const prepare = gulp.series([]);
 const assets = gulp.series([]);
 const live = gulp.parallel([]);
 
-export const dev = gulp.series([]);
+export const dev = gulp.series([prepare, assets, live]);
