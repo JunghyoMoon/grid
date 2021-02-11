1eee367e587b8571f9c851bdfd8305574ce849b7
JungHyoMoon <dsinhome70@gmail.com>
2021-02-11 22:02:28 +0900

    scss setting is done

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,7 @@ import minify from "gulp-csso";
 import img from "gulp-image";
 import babelify from "babelify";
 import uglify from "uglifyify";
+import autoPrefixer from "gulp-autoprefixer";
 
 const routes = {
     img: {
@@ -25,8 +26,20 @@ const routes = {
     },
 };
 
+const style = () =>
+    gulp
+        .src(routes.scss.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(
+            autoPrefixer({
+                browsers: ["last 2 versions"],
+            })
+        )
+        .pipe(minify())
+        .pipe(gulp.dest(routes.scss.dest));
+
 const prepare = gulp.series([]);
-const assets = gulp.series([]);
+const assets = gulp.series([style]);
 const live = gulp.parallel([]);
 
 export const dev = gulp.series([prepare, assets, live]);
