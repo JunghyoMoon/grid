3147dc9c8c8cff2963a801082629ac4094343a56
JungHyoMoon <dsinhome70@gmail.com>
2021-02-17 22:14:39 +0900

    working on modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,21 @@
-import doc from "./variables";
+import { doc } from "./variables";
 
-const handleClick = () => {};
+const closeWindow = (event) => {};
+
+const handleClick = (event) => {
+    const target = event.target;
+    const src = target.getAttribute("src");
+    const bg = document.createElement("div");
+    const img = document.createElement("img");
+    img.setAttribute("src", src);
+    const closeBtn = document.createElement("button");
+    closeBtn.innerText = "❌";
+};
 
 const init = () => {
     doc.items.forEach((item) => item.addEventListener("click", handleClick));
 };
 
-if (container) {
+if (doc.container) {
     init();
 }
--- src/js/variables.js
+++ src/js/variables.js
@@ -1,7 +1,5 @@
-const doc = {
+export const doc = {
     items: document.querySelectorAll(".item"),
     container: document.querySelector("section"),
     body: document.querySelector("body"),
 };
-
-export default doc;
